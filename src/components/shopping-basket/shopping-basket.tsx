34b5ce701d18e847be9289390fdd92cc1e1ad7d2
Irfan Undevli <irfan@kodstrukt.com>
2021-03-14 22:05:13 +0000

    Added styling to shopping basket component

--- src/components/shopping-basket/shopping-basket.tsx
+++ src/components/shopping-basket/shopping-basket.tsx
@@ -1,17 +1,25 @@
 import { useRecoilValue } from "recoil";
 import { shoppingBasketItemsState } from "./shopping-basket.state";
+import "./shopping-basket.scss";
 
 export const ShoppingBasket: React.FC = () => {
   const itemsInBasket = useRecoilValue(shoppingBasketItemsState);
 
   return (
-    <div>
-      <div>Shopping Basket</div>
+    <div className="c-shopping-basket">
+      <h3>Shopping Basket</h3>
       {itemsInBasket.length > 0 &&
         itemsInBasket.map((item, index) => {
           return (
-            <div key={`${item.id}-${index}`}>
-              <img src={item.imageUrl} alt={item.name} />
+            <div
+              key={`${item.id}-${index}`}
+              className="c-shopping-basket__item"
+            >
+              <img
+                className="c-shopping-basket__item-image"
+                src={item.imageUrl}
+                alt={item.name}
+              />
               <div>{item.name}</div>
               <div>{item.price}</div>
             </div>
